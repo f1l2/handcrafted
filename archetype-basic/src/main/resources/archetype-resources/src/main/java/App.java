401eeaeb0dac408d572e3ea6513a5a86ba4e2dfe
Manuel <manuel.filz@gmx.at>
2016-12-23 18:52:46 +0100

    initial commit

---
+++ archetype-basic/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $at.f1l2.mvn.archetype.basic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
